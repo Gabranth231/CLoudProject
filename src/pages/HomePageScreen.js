2e4618eddc67fa688953ee1a26ba8e049dc24615
Gabranth231 <boycie3164@gmail.com>
2021-11-13 20:58:55 +0000

    Working page

--- src/pages/HomePageScreen.js
+++ src/pages/HomePageScreen.js
@@ -3,14 +3,18 @@ import { useState } from 'react';
 
 function HomePageScreen() {
   const [widgetCount, setWidget] = useState(0);
-
+  function checkCount(){
+    if(widgetCount>0){
+      setWidget(widgetCount-1);
+    }
+  }
   return (
     <div className={classes.mainDiv}>
       <div className={classes.widgetcontainer}>
         <div>Widget count is {widgetCount}</div>
         <div className={classes.inputcontainer}>
           <div onClick={() => setWidget(widgetCount + 1)}>Add Widget</div>
-          <div onClick ={() => setWidget(widgetCount - 1)}>Delete Widget</div>
+          <div onClick ={() => checkCount()}>Delete Widget</div>
         </div>
       </div>
     </div>
