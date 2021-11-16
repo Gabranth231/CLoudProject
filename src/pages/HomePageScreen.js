b694bacb4e1a7f4bd2559caa41a39b7a92d34f64
josbrink <G00362643@gmit.ie>
2021-11-16 19:18:27 +0000

    changed CSS and added nav bar

--- src/pages/HomePageScreen.js
+++ src/pages/HomePageScreen.js
@@ -4,17 +4,21 @@ import { useState } from 'react';
 function HomePageScreen() {
   const [widgetCount, setWidget] = useState(0);
   function checkCount(){
-    if(widgetCount>0){
-      setWidget(widgetCount-1);
-    }
+      widgetCount > 0 ? setWidget(widgetCount-1) : setWidget(widgetCount);
   }
   return (
     <div className={classes.mainDiv}>
-      <div className={classes.widgetcontainer}>
-        <div>Widget count is {widgetCount}</div>
-        <div className={classes.inputcontainer}>
+      <div className={classes.counter}>
+        Widget count is {widgetCount}
+      </div>
+
+      <div className={classes.buttons}>
+        <div>
           <div onClick={() => setWidget(widgetCount + 1)}>Add Widget</div>
-          <div onClick ={() => checkCount()}>Delete Widget</div>
+        </div>
+
+        <div>
+         <div onClick ={() => checkCount()}>Delete Widget</div>
         </div>
       </div>
     </div>
